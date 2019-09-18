5aff7aa45cf0d76d9cafdf964af49bf0c3fbc3b3
rsporteman <raonibs@gmail.com>
2019-09-18 16:56:39 -0300

    Cors Middleware

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['verify' => true]);
-
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::post('/login', 'Auth\LoginController@login')->middleware(['web', 'guest']);
+Route::get('/login', 'Auth\LoginController@showLoginForm')->middleware(['web', 'guest'])->name('login');
+Route::post('/logout', 'Auth\LoginController@logout')->middleware(['web'])->name('logout');
+
+Route::get('/email/resend', 'Auth\VerificationController@resend')->name('verification.resend')->middleware(['web', 'auth', 'throttle:6,1']);
+Route::get('/email/verify  ', 'Auth\VerificationController@show')->name('verification.notice')->middleware(['web', 'auth']);
+Route::get('/email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify')->middleware(['web', 'auth', 'signed', 'throttle:6,1']);
+Route::post('/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email')->middleware(['web', 'guest']);
+Route::get('/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request')->middleware(['web', 'guest']);
+Route::post('/password/reset', 'Auth\ResetPasswordController@reset')->name('password.update')->middleware(['web', 'guest']);
+Route::get('/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset')->middleware(['web', 'guest']);
+Route::get('/register', 'Auth\RegisterController@showRegistrationForm')->name('register')->middleware(['web', 'guest']);
+Route::post('/register', 'Auth\RegisterController@register')->middleware(['web', 'guest']);
+
